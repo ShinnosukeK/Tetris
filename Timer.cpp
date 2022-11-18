5054f97f13bd62cb03a1e3776b641acc15c942da
kitashin <k.shinnosuke0603@gmail.com>
2022-11-18 18:31:11 +0900

    added FPS counting scripts

---
+++ Timer.cpp
@@ -0,0 +1,40 @@
+#include "Timer.hpp"
+
+void Timer::InitTimer() {
+	time = GetNowHiPerformanceCount();//ゲームループに入る前の時刻保存
+	fpsCheckTime = GetNowHiPerformanceCount();//初回のFPS計測の際の基準時刻
+	fps = 0;
+	fpsCount = 0;
+	deltaTime = 0.000001f;//仮の値
+}
+
+void Timer::CountFPS() {
+
+	//現時刻保存
+	nowTime = GetNowHiPerformanceCount();
+	
+	//経過時間計測
+	deltaTime = (nowTime - time) / 1000000.0f;
+
+	//次のフレームで使う基準時刻として保存	
+	time = nowTime;
+
+	fpsCount++;
+
+	//現時刻-計測開始時刻が1sを超えたら
+	if (nowTime - fpsCheckTime > 1000000) {
+
+		//FPSを決定
+		fps = fpsCount;		
+
+		//FPS計測変数をリセット
+		fpsCount = 0;
+
+		//次回のFPS計測の際の基準時刻
+		fpsCheckTime = nowTime;
+	}
+}
+
+int Timer::GetFPS() {
+	return this->fps;
+}
---
+++ Timer.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include "DxLib.h"
+
+class Timer {
+private:
+	LONGLONG nowTime;//現フレームの時刻
+	LONGLONG time;//1フレーム前の時刻
+	LONGLONG fpsCheckTime;//FPS計測用のスタート時刻
+	float deltaTime;//フレーム間の時間
+	int fps;
+	int fpsCount;//経過フレームを数えていくための変数
+
+public:
+	/// <summary>
+	/// タイマーの初期化
+	/// </summary>
+	void InitTimer();
+
+	/// <summary>
+	/// FPSを計測（毎フレーム呼び出し続けること）
+	/// </summary>
+	void CountFPS();
+
+	int GetFPS();
+};
+
